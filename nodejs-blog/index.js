4d3c61b4da3d07cb5b99ffda9a7e0734c06f8923
suhana <suhana.ayisha@dubizzle.com>
2019-11-06 14:48:14 +0400

    removing node modules from git

--- nodejs-blog/index.js
+++ nodejs-blog/index.js
@@ -1,7 +1,12 @@
+const path = require('path')
 const express = require('express')
 
 const app = new express()
 
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+})
+app.use(express.static('public'))
 app.listen(4000,() => {
     console.log("listening on port 4000")
 })
