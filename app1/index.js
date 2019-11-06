16ea50a88060369d9b5896078447197c1fe07777
suhana <suhana.ayisha@dubizzle.com>
2019-11-06 11:39:46 +0400

    diff responses for diff url requests

--- app1/index.js
+++ app1/index.js
@@ -6,7 +6,17 @@ const http = require('http') //grabs http package and store it in a variable cal
 const server= http.createServer((request,response)=>{
     console.log(request.url)
 
-    response.end('HELLO NODE JS') //responds with hello nodejs
+    if (request.url === '/about'){
+        return response.end('The about page')
+    } else if (request.url === '/contact'){
+        return response.end('The contact page')
+    } else if (request.url === '/'){
+        return response.end('The home page')
+    } else {
+        response.writeHead(404)
+        response.end('Not found')
+    }
+
 })
 
 //start the server so that browser can make request to it
