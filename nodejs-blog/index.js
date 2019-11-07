2a699c6236bbbf8eb438f20c0a62c666cae6dd34
suhana <suhana.ayisha@dubizzle.com>
2019-11-07 15:20:39 +0400

    creating a post form, output its data

--- nodejs-blog/index.js
+++ nodejs-blog/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const { config, engine } = require('express-edge'); //for templating
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser') // to collect data from browser
 
 const app = new express()
 
@@ -13,10 +14,22 @@ app.use(engine);
 
 app.set('views', `${__dirname}/views`);
 
+app.use(bodyParser.json()) 
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (request, response) => {
     response.render('index')
     // response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+})
+
+app.get('/posts/new', (request, response) => {
+    response.render('create')
+})
+
+app.post('/posts/store', (request, response) => {
+    console.log(request.body) //body-parser has a function 'body' that contains an obj with all data coming from browser
+    response.redirect('/')
 })
 
 app.get('/index', (request, response) => {
