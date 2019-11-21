ccbd2820a759cb6f8d80c0d972fa0c3100daeeb4
Suhana Ayisha <suhanaayisha@gmail.com>
2019-11-21 13:46:05 +0400

    removing contractor-supplier from here

--- contractor-supplier/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App1 from './components/App1';
-import AppRouter from './routers/AppRouter';
-
-
-ReactDOM.render(<AppRouter/>, document.getElementById('root'));
